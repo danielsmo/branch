b0e0281c5be27829ff8f9386e288b2c7a8d9877a
danielsmo <58270989+danielsmo@users.noreply.github.com>
2023-03-30 19:22:35 -0300

    buu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,14 +3,4 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("daniel 1");
-<<<<<<< HEAD
-
-    }
-
-    private void metodo(){
-        String azul;
-        
-=======
->>>>>>> parent of af43068 (commit 2)
-    }
 }
